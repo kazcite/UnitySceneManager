6888fc180fa6a9b4427852b8e600a14e2e899136
mrbeen404 <kazcite@gmail.com>
2013-12-09 23:07:08 +0900

    TODOを追加

--- testProj/Assets/SceneManager.cs
+++ testProj/Assets/SceneManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
+// TODO:シーン自体が非アクティブ化されるときはシーンの子要素のアクティブ状態を記憶し、シーンがアクティブ化されるときには記憶した子要素の状態を復帰させる
+
 public class SceneManager : MonoBehaviour {
 	
 	Hashtable ht = new Hashtable();
