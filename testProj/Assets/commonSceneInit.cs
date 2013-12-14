c9bb7760d92936cca6418112e73e4f5a51141804
mrbeen404 <kazcite@gmail.com>
2013-12-15 05:10:24 +0900

    commonシーンにフェード用スプライト追加

---
+++ testProj/Assets/commonSceneInit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class commonSceneInit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		GameObject go = GameObject.Find ("fadeScreen" );
+		UISprite sprite = (UISprite)go.GetComponent ("UISprite");
+		sprite.alpha = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
